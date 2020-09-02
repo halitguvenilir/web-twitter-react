bf8c9de623ae1cb839d637674caef8bc1a07a37f
Halit <halitguvenilir@gmail.com>
2020-09-02 18:04:59 +0300

    Postcss and navigation added

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 import Button from '../components/button'
+import NavigationButton from '../components/navigation-button'
+import Navigation from '../components/navigation'
 
 export default {
   title: 'Buttons'
 }
 
 export const NormalButton = () => <Button>Save</Button>
+
+export const NavButton = () => <NavigationButton>Menu Button</NavigationButton>
+
+export const Nav = () => <Navigation />
